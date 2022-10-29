69f9cf3d6dd632e0423cb61fa3489c94d36ec6e8
f_miguel <fabiosemedo200@gmail.com>
2022-10-29 12:46:12 +0100

    nvim: fix dap-python bin path
    Using python3 instead of just python since in debian or ubuntu there's
    no sinlink python -> python3

--- .config/nvim/lua/debugging.lua
+++ .config/nvim/lua/debugging.lua
@@ -2,7 +2,7 @@ local mod = {}
 
 function mod.setup()
     require('keybinds').set_dap_keybinds()
-    require('dap-python').setup('python')
+    require('dap-python').setup('python3')
     require('dap-python').test_runner = 'pytest'
     require('dapui').setup()
     require('nvim-dap-virtual-text').setup()
