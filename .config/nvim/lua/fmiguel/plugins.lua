473dbf843a8102d8a62a2cec81c2818c37db2624
f_miguel <fabiosemedo200@gmail.com>
2023-01-10 23:28:10 +0000

    nvim: Migrate from Packer to Lazy.nvim

--- .config/nvim/lua/fmiguel/plugins.lua
+++ .config/nvim/lua/fmiguel/plugins.lua
@@ -1,85 +1,97 @@
-local plugins = {}
+local M = {}
 
-function plugins.load()
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
+function M.load()
+return require('lazy').setup {
     -- When a file is opened, this will switch the vim workspace to the project's root
-    use 'airblade/vim-rooter'
+    'airblade/vim-rooter',
 
     -- git support
-    use 'lewis6991/gitsigns.nvim'
+    'lewis6991/gitsigns.nvim',
 
     -- highlights yanked region
-    use 'machakann/vim-highlightedyank'
+    'machakann/vim-highlightedyank',
 
     -- treesitter provides better syntax highlight
-    use {
+    {
         'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+        build = ':TSUpdate'
+    },
 
     -- Telescope
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-    use 'nvim-telescope/telescope-file-browser.nvim'
-    use 'kyazdani42/nvim-web-devicons'
+    {
+        'nvim-telescope/telescope.nvim', version = '0.1.0',
+        dependencies = {
+            {'nvim-lua/plenary.nvim'},
+            {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+            'nvim-telescope/telescope-file-browser.nvim',
+            'kyazdani42/nvim-web-devicons',
+        }
+    },
 
     -- Configurations for Nvim LSP, DAP and Linters
-    use 'williamboman/mason.nvim'
-    use 'williamboman/mason-lspconfig.nvim'
-    use 'jayp0521/mason-nvim-dap.nvim'
-    use 'neovim/nvim-lspconfig'
-    use 'mfussenegger/nvim-dap'
-    use 'rcarriga/nvim-dap-ui'
-    use 'theHamsta/nvim-dap-virtual-text' -- inline values
-    use 'mfussenegger/nvim-dap-python'
-    use 'j-hui/fidget.nvim' -- feedback while I wait for rust-analyzer
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'jayp0521/mason-nvim-dap.nvim',
+    'neovim/nvim-lspconfig',
+    'mfussenegger/nvim-dap',
+    'rcarriga/nvim-dap-ui',
+    'theHamsta/nvim-dap-virtual-text', -- inline values
+    'mfussenegger/nvim-dap-python',
+    'j-hui/fidget.nvim', -- feedback while I wait for rust-analyzer
 
     -- lsp auto-completion
-    use 'hrsh7th/nvim-cmp' -- the completion engine
-    use 'hrsh7th/cmp-buffer' -- words from the curr buffer
-    use 'hrsh7th/cmp-path' -- path auto-completion
-    use 'hrsh7th/cmp-nvim-lsp' -- lsp
-    use 'hrsh7th/cmp-nvim-lua' -- nvim lua api
-    use 'L3MON4D3/LuaSnip' -- snippets engine
-    use 'saadparwaiz1/cmp_luasnip' -- the luasnip driver for cmp
+    'hrsh7th/nvim-cmp', -- the completion engine
+    'hrsh7th/cmp-buffer', -- words from the curr buffer
+    'hrsh7th/cmp-path', -- path auto-completion
+    'hrsh7th/cmp-nvim-lsp', -- lsp
+    'hrsh7th/cmp-nvim-lua', -- nvim lua api
+    'L3MON4D3/LuaSnip', -- snippets engine
+    'saadparwaiz1/cmp_luasnip', -- the luasnip driver for cmp
 
-    use 'windwp/nvim-autopairs' -- auto-close pairs
+    'windwp/nvim-autopairs', -- auto-close pairs
 
-    use {
+    {
         'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
+        dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true }
+    },
 
     -- proper tabs
-    use {
+    {
         'akinsho/bufferline.nvim',
-        tag = 'v3.*',
-        requires = 'kyazdani42/nvim-web-devicons'
-    }
+        version = 'v3.*',
+        dependencies = 'kyazdani42/nvim-web-devicons'
+    },
 
     -- netrw icons
-    use 'prichrd/netrw.nvim'
+    'prichrd/netrw.nvim',
 
     -- themes
-    use 'Mofiqul/vscode.nvim'
-    use 'folke/tokyonight.nvim'
-    use {
+    'Mofiqul/vscode.nvim',
+    'folke/tokyonight.nvim',
+    {
         'catppuccin/nvim',
-        as = 'catppuccin',
-    }
-    use({
+        name = 'catppuccin',
+    },
+    {
         'rose-pine/neovim',
-        as = 'rose-pine',
-    })
-    use 'vimpostor/vim-lumen' -- auto dark mode
-end)
+        name = 'rose-pine',
+    },
+    'vimpostor/vim-lumen', -- auto dark mode
+}
 end
 
-return plugins
+return M
 
