d30497da699cc5fe8f8fbe30341537c5dca8f0c3
f_miguel <fabiosemedo200@gmail.com>
2023-01-24 06:11:33 +0000

    nvim: Better keybinds

--- .config/nvim/lua/fmiguel/keybinds.lua
+++ .config/nvim/lua/fmiguel/keybinds.lua
@@ -6,13 +6,13 @@ end
 
 
 function keybinds.set_general_keybinds()
-    nmap('<leader>ff', "<cmd>lua require('telescope.builtin').find_files({hidden=true})<CR>")
+    nmap('<leader>f', "<cmd>lua require('telescope.builtin').find_files({hidden=true})<CR>")
     nmap('<leader>sw', "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>")
     nmap('<leader>sd', "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
     nmap('<leader>dg', "<cmd>:Telescope diagnostics<CR>")
-    nmap('<leader>gg', "<cmd>:Telescope live_grep<CR>")
+    nmap('<leader>g', "<cmd>:Telescope live_grep<CR>")
     nmap('<leader>E', ":Ex<CR>")
-    nmap('<leader>fe', ":Telescope file_browser<CR>")
+    nmap('<leader>e', ":Telescope file_browser<CR>")
     nmap('<leader>w', "<cmd>lua vim.cmd('w')<CR>")
     nmap('<leader>e', "<cmd>lua vim.diagnostic.open_float()<CR>")
 end
@@ -37,10 +37,10 @@ function keybinds.set_lsp_keybinds()
     nmap('<leader>gd', vim.lsp.buf.definition, {buffer = 0})
     nmap('<leader>gt', vim.lsp.buf.type_definition, {buffer = 0})
     nmap('<leader>gi', vim.lsp.buf.implementation, {buffer = 0})
+    nmap('<leader>gr', '<cmd>:Telescope lsp_references<CR>', {buffer = 0})
     nmap('<leader>dj', vim.diagnostic.goto_next, {buffer = 0})
     nmap('<leader>dk', vim.diagnostic.goto_prev, {buffer = 0})
     nmap('<leader>R', vim.lsp.buf.rename, {buffer = 0})
-    nmap('<leader>r', '<cmd>:Telescope lsp_references<CR>', {buffer = 0})
     nmap('<leader>a', vim.lsp.buf.code_action, {buffer = 0})
 end
 
