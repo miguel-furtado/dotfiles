4fa810f9fa3b350c9da1a48f3400c611d384ea0b
f_miguel <fabiosemedo200@gmail.com>
2023-04-10 15:16:26 +0100

    nvim: Lualine show relative path instead of just file name

--- .config/nvim/lua/fmiguel/pconfig/lualine.lua
+++ .config/nvim/lua/fmiguel/pconfig/lualine.lua
@@ -8,7 +8,7 @@ function M.setup()
     sections = {
       lualine_a = {'mode'},
       lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
+      lualine_c = {{'filename', path = 1}},
       lualine_x = {},
       lualine_y = {'progress'},
       lualine_z = {'location'}
