7e2d4a5a0bb9cc97f493cc12305a77814a6cadc9
f_miguel <fabiosemedo200@gmail.com>
2023-04-25 15:40:21 +0100

    Add harpoon keybinds

--- .config/nvim/lua/fmiguel/keybinds.lua
+++ .config/nvim/lua/fmiguel/keybinds.lua
@@ -17,10 +17,10 @@ function keybinds.set_general_keybinds()
 
   nmap('<leader>hh', "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>")
   nmap('<leader>ha', "<cmd>lua require('harpoon.mark').add_file()<CR>")
-  nmap('<leader>11', "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
-  nmap('<leader>22', "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
-  nmap('<leader>33', "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
-  nmap('<leader>44', "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
+  nmap('<leader>1', "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
+  nmap('<leader>2', "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
+  nmap('<leader>3', "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
+  nmap('<leader>4', "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
 end
 
 function keybinds.set_dap_keybinds()
