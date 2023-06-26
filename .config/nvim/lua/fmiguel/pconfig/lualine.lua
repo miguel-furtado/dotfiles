8091cd69ee53316f1cc37f1768a57107093d7daa
f_miguel <fabiosemedo200@gmail.com>
2023-06-26 23:48:49 +0100

    nvim: Show filetype in lualine

--- .config/nvim/lua/fmiguel/pconfig/lualine.lua
+++ .config/nvim/lua/fmiguel/pconfig/lualine.lua
@@ -9,7 +9,7 @@ function M.setup()
       lualine_a = {"mode"},
       lualine_b = {"branch", "diff", "diagnostics"},
       lualine_c = {{"filename", path = 1}},
-      lualine_x = {},
+      lualine_x = {"filetype"},
       lualine_y = {"progress"},
       lualine_z = {"location"}
     },
