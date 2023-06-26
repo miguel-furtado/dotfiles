24688af8040a6003fbeeae3c61f1bfaebd6d2c20
f_miguel <fabiosemedo200@gmail.com>
2023-06-26 02:26:39 +0100

    nvim: Clean cmp config

--- .config/nvim/lua/fmiguel/pconfig/cmp.lua
+++ .config/nvim/lua/fmiguel/pconfig/cmp.lua
@@ -11,37 +11,28 @@ function M.setup()
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+        require("luasnip").lsp_expand(args.body)
       end,
     },
     mapping = cmp.mapping.preset.insert({
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
       ["<c-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
       { name = "nvim_lsp" },
       { name = "nvim_lua" },
-      { name = "luasnip" }, -- For luasnip users.
+      { name = "luasnip" },
     }, {
       { name = "path" },
       { name = "buffer" },
     }),
     experimental = {
       ghost_text = true,
-    }
-  })
-
-  -- Set configuration for specific filetype.
-  cmp.setup.filetype("gitcommit", {
-    sources = cmp.config.sources({
-      { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-      { name = "buffer" },
-    })
+    },
   })
 end
 
 return M
+
