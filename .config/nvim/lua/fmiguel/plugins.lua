6870c810492d3f318d0a80f41bef7d9268398cd5
f_miguel <fabiosemedo200@gmail.com>
2023-07-08 20:04:11 +0000

    nvim: Simplify config
    Remove the autopairs plugin dedicated file since the config is not
    large.

--- .config/nvim/lua/fmiguel/plugins.lua
+++ .config/nvim/lua/fmiguel/plugins.lua
@@ -153,7 +153,14 @@ local plugins = {
 
   {
     "windwp/nvim-autopairs", -- auto-close pairs
-    config = require"fmiguel.pconfig.autopairs".setup,
+    config = function()
+      require("nvim-autopairs").setup {
+        check_ts = true,
+        disable_filetype = { "TelescopePrompt" },
+        -- do not trigger if a close pair is in the same line
+        enable_check_bracket_line = false,
+      }
+    end,
     event = "InsertEnter",
   },
 
