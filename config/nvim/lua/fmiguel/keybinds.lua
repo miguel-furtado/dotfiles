e11869fd11a7058a62613f656ffe38a86c6f2dc7
f_miguel <fabiosemedo200@gmail.com>
2023-08-29 01:06:29 +0100

    nvim: Bye, dap

--- config/nvim/lua/fmiguel/keybinds.lua
+++ config/nvim/lua/fmiguel/keybinds.lua
@@ -3,60 +3,39 @@ local function nmap(keys, action)
   vim.keymap.set("n", keys, action, {silent=true})
 end
 
-function set_general_keybinds()
-  -- general
-  nmap("<leader>w", "<cmd>lua vim.cmd('w')<CR>")
-  nmap("<leader>ee", "<cmd>lua vim.diagnostic.open_float()<CR>")
-  nmap("<leader>E", "<cmd>lua vim.cmd('Ex')<CR>")
+-- general
+nmap("<leader>w", "<cmd>lua vim.cmd('w')<CR>")
+nmap("<leader>ee", "<cmd>lua vim.diagnostic.open_float()<CR>")
+nmap("<leader>E", "<cmd>lua vim.cmd('Ex')<CR>")
 
-  -- telescope
-  nmap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
-  nmap("<leader>sw", "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>")
-  nmap("<leader>ss", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
-  nmap("<leader>dg", "<cmd>lua require('telescope.builtin').diagnostics()<CR>")
-  nmap("<leader>gg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
-  nmap("<leader>gr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>")
+-- telescope
+nmap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
+nmap("<leader>sw", "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>")
+nmap("<leader>ss", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
+nmap("<leader>dg", "<cmd>lua require('telescope.builtin').diagnostics()<CR>")
+nmap("<leader>gg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
+nmap("<leader>gr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>")
 
-  -- harpoon
-  nmap("<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>")
-  nmap("<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>")
-  nmap("<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
-  nmap("<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
-  nmap("<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
-  nmap("<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
-end
+-- harpoon
+nmap("<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>")
+nmap("<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<CR>")
+nmap("<leader>1", "<cmd>lua require('harpoon.ui').nav_file(1)<CR>")
+nmap("<leader>2", "<cmd>lua require('harpoon.ui').nav_file(2)<CR>")
+nmap("<leader>3", "<cmd>lua require('harpoon.ui').nav_file(3)<CR>")
+nmap("<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>")
 
-function set_dap_keybinds()
-  nmap("<F5>", ":lua require('dap').continue()<CR>")
-  nmap("<F2>", ":lua require('dap').step_over()<CR>")
-  nmap("<F3>", ":lua require('dap').step_into()<CR>")
-  nmap("<F4>", ":lua require('dap').step_out()<CR>")
-  nmap("<leader><F5>", ":lua require('dap').close()<CR>")
-  nmap("<leader>B", ":lua require('dap').toggle_breakpoint()<CR>")
-  -- conditional breakpoint
-  nmap("<leader>cB", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-end
-
-function set_lsp_keybinds()
-  -- `buffer 0` means whatever the current buffer is so the shortcuts only
-  -- work on files that trigger the lsp
-  nmap("K", vim.lsp.buf.hover)
-  nmap("<leader>gd", vim.lsp.buf.definition)
-  nmap("<leader>gt", vim.lsp.buf.type_definition)
-  nmap("<leader>gi", vim.lsp.buf.implementation)
-  nmap("<leader>dj", vim.diagnostic.goto_next)
-  nmap("<leader>dk", vim.diagnostic.goto_prev)
-  nmap("<leader>R", vim.lsp.buf.rename)
-  nmap("<leader>a", vim.lsp.buf.code_action)
-end
-
-set_general_keybinds()
-
+-- only set when an lsp is attached
 vim.api.nvim_create_autocmd({"LspAttach"}, {
   pattern = {"*"},
   callback = function()
-    set_lsp_keybinds()
-    set_dap_keybinds()
+    nmap("K", vim.lsp.buf.hover)
+    nmap("<leader>gd", vim.lsp.buf.definition)
+    nmap("<leader>gt", vim.lsp.buf.type_definition)
+    nmap("<leader>gi", vim.lsp.buf.implementation)
+    nmap("<leader>dj", vim.diagnostic.goto_next)
+    nmap("<leader>dk", vim.diagnostic.goto_prev)
+    nmap("<leader>R", vim.lsp.buf.rename)
+    nmap("<leader>a", vim.lsp.buf.code_action)
   end
 })
 
--- config/nvim/lua/fmiguel/plugins/mason.lua
+++ config/nvim/lua/fmiguel/plugins/mason.lua
@@ -9,12 +9,6 @@ local lsp_servers = {
   "jsonls",
 }
 
-local dap_servers = {
-  "python", -- debugpy
-  "codelldb", -- rust
-  "delve", -- golang
-}
-
 local ft = {
   "rust",
   "go",
@@ -46,35 +40,14 @@ function setup_lsp(servers)
   )
 end
 
-function setup_dap(servers)
-  local dap, widgets = require"dap", require"dap.ui.widgets"
-  require"mason-nvim-dap".setup {
-    ensure_installed = servers,
-    automatic_setup = true,
-  }
-
-  local widget = widgets.sidebar(widgets.scopes)
-  dap.listeners.after.event_initialized["dap"] = function()
-    widget.open()
-  end
-  dap.listeners.before.event_terminated["dap"] = function()
-    widget.close()
-  end
-  dap.listeners.before.event_exited["dap"] = function()
-    widget.close()
-  end
-end
-
 function setup()
   require"mason".setup {
       PATH = "prepend",
   }
   setup_lsp(lsp_servers)
-  setup_dap(dap_servers)
 end
 
 return {
-  -- Configurations for Nvim LSP, DAP and Linters
   {
     "williamboman/mason.nvim",
     version = "^1",
@@ -82,33 +55,14 @@ return {
     ft = ft,
     event = "CmdUndefined Mason",
     dependencies = {
-      -- provides lspconfig compatibility to mason
       {
+        -- provides lspconfig compatibility to mason
         "williamboman/mason-lspconfig.nvim",
         version = "0.x.x",
-        dependencies = {
-          {
-           "neovim/nvim-lspconfig",
-            version = "0.x.x",
-          },
-        },
       },
       {
-        "jayp0521/mason-nvim-dap.nvim",
-        version = "^2",
-        dependencies = {
-          "mfussenegger/nvim-dap",
-          version = "0.x.x",
-        },
-      },
-      {
-        -- need this because the mason setup does not include running
-        -- delve with args
-        "leoluz/nvim-dap-go",
-        ft = "go",
-        config = function()
-          require("dap-go").setup()
-        end,
+        "neovim/nvim-lspconfig",
+        version = "0.x.x",
       },
     },
   },
