20ab4e3675bc6520a133b86a2fd3fc493ef99d28
f_miguel <fabiosemedo200@gmail.com>
2023-09-05 01:53:38 +0100

    nvim: Treesitter lazy compile

--- config/nvim/lua/fmiguel/plugins/treesitter.lua
+++ config/nvim/lua/fmiguel/plugins/treesitter.lua
@@ -6,7 +6,6 @@ return {
     event = "BufRead",
     config = function()
       require"nvim-treesitter.configs".setup {
-        ensure_installed = "all",
         sync_install = false,
         auto_install = true,
         highlight = {
