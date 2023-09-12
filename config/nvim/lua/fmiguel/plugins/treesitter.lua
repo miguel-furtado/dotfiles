e37e3474d30d75468493f05fb5f5a01f09205f81
f_miguel <fabiosemedo200@gmail.com>
2023-09-12 19:55:03 +0100

    nvim: Fix treesitter settings

--- config/nvim/lua/fmiguel/plugins/treesitter.lua
+++ config/nvim/lua/fmiguel/plugins/treesitter.lua
@@ -10,8 +10,8 @@ return {
         auto_install = true,
         highlight = {
           enable = true,
+          additional_vim_regex_highlighting = false,
         },
-        additional_vim_regex_highlighting = false,
       }
     end,
   },
