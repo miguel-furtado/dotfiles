73d4a02109cab545f9bf16e261cc7ca262140dea
f_miguel <fabiosemedo200@gmail.com>
2023-12-10 05:05:02 +0000

    Use default gitsigns options

--- config/nvim/lua/fmiguel/plugins/gitsigns.lua
+++ config/nvim/lua/fmiguel/plugins/gitsigns.lua
@@ -2,15 +2,7 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     version = "0.x.x",
-    opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-    }
+    opts = {},
   },
 }
 
