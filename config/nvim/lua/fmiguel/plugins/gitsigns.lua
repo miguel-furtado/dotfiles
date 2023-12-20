787b9d0b4489343869ebe7c9724bb08462951ea1
f_miguel <fabiosemedo200@gmail.com>
2023-12-20 19:53:15 +0000

    Revert "Use default gitsigns options"
    This reverts commit bfee96ea556a53f0244cb26a513f11d0b7e1a819.

--- config/nvim/lua/fmiguel/plugins/gitsigns.lua
+++ config/nvim/lua/fmiguel/plugins/gitsigns.lua
@@ -2,7 +2,15 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     version = "0.x.x",
-    opts = {},
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    }
   },
 }
 
