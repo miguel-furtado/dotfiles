6df7dfddd6ab3f35d6b466089d6403340eaffa77
f_miguel <fabiosemedo200@gmail.com>
2024-01-04 01:55:15 +0000

    nvim: Remove unnecessary treesitter settings
    sync_install only applies to ensure_installed which I'm not using and
    the rest is already default, so there's no need to set explicitly.

--- config/nvim/lua/fmiguel/plugins/treesitter.lua
+++ config/nvim/lua/fmiguel/plugins/treesitter.lua
@@ -4,15 +4,9 @@ return {
   build = ":TSUpdate",
   config = function()
     require"nvim-treesitter.configs".setup {
-      sync_install = false,
       auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
+      highlight = { enable = true },
+      indent = { enable = true },
     }
   end,
 }
