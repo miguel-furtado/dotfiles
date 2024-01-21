8f4bb00c383e0a81138d07be683e329ec444d944
f_miguel <fabiosemedo200@gmail.com>
2024-01-21 01:58:47 +0000

    zsh: Clean aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,17 +1,12 @@
-alias ll='ls -lh'
-alias la='ls -lAh'
+alias ll="ls -lh"
+alias la="ls -lAh"
 alias dbox="distrobox"
 
 # some colour plz
-alias ls='ls --color=auto'
-alias grep='grep --color'
+alias ls="ls --color=auto"
+alias grep="grep --color"
 
 if [[ $DISTRO_FAMILY = "debian" || $DISTRO = "debian" ]]; then
     alias bat="batcat"
 fi
 
-if [[ $TERM = "xterm-kitty" ]]; then
-    alias ssh="kitty +kitten ssh"
-fi
-
-alias dev="distrobox enter dev"
