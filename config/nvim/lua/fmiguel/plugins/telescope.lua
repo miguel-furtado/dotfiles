4467fc86ceda92b85c038448f71277618a80fb3f
f_miguel <fabiosemedo200@gmail.com>
2024-07-13 01:53:36 +0100

    nvim: update telescope settings

--- config/nvim/lua/fmiguel/plugins/telescope.lua
+++ config/nvim/lua/fmiguel/plugins/telescope.lua
@@ -3,12 +3,20 @@ return {
   branch = '0.1.x',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    {'<leader>ff', function() require('telescope.builtin').find_files() end},
+    {'<leader>sf', function() require('telescope.builtin').find_files() end},
     {'<leader>gg', function() require('telescope.builtin').live_grep() end},
     {'<leader>gr', function() require('telescope.builtin').lsp_references() end},
     {'<leader>ss', function() require('telescope.builtin').lsp_document_symbols() end},
-    {'<leader>ws', function() require('telescope.builtin').lsp_workspace_symbols() end},
+    {'<leader>sw', function() require('telescope.builtin').lsp_workspace_symbols() end},
     {'<leader>dg', function() require('telescope.builtin').diagnostics() end},
-  }
+  },
+	opts = {
+		defaults = {
+			file_ignore_patterns = { ".git/", "node_modules/" }
+      },
+      pickers = {
+        find_files = { hidden = true },
+      },
+	}
 }
 
