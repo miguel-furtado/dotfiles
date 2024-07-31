174236c2c64872d93ca619dedda280ca9d0bb269
f_miguel <fabiosemedo200@gmail.com>
2024-07-31 23:57:15 +0100

    refactor(nvim): use default color-scheme
    Get rid of installed color-schemes and use built-in instead.

--- config/nvim/lua/fmiguel/plugins/colors.lua
+++ config/nvim/lua/fmiguel/plugins/colors.lua
@@ -1,16 +1,1 @@
-return {
-	{ "vimpostor/vim-lumen" },
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				dark_variant = "moon", -- main, moon, or dawn
-				styles = {
-					italic = false,
-				},
-			})
-			vim.cmd.colorscheme("rose-pine")
-		end,
-	},
-}
+return { "vimpostor/vim-lumen" }
