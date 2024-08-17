c0f0986d92daa5bb172cca39ce7bd4178ce626e6
f_miguel <fabiosemedo200@gmail.com>
2024-08-17 06:51:09 +0100

    nvim: Revamp neovim options

--- config/nvim/lua/fmiguel/options.lua
+++ config/nvim/lua/fmiguel/options.lua
@@ -1,14 +1,13 @@
 vim.o.number = true
 vim.o.spell = true
-vim.o.mouse = "a" --enable mouse cursor
+vim.o.mouse = "a" -- enable mouse cursor
 vim.o.encoding = "UTF-8"
 vim.o.hlsearch = false
 vim.o.laststatus = 3
 vim.opt.scrolloff = 8
 vim.opt.swapfile = false
-
--- Show ghost chars for some characters like tab an leading space.
-vim.opt.list = true
+vim.wo.wrap = false
+vim.opt.list = true -- show ghost characters for tabs and leading spaces
 
 -- Netrw
 vim.g.netrw_banner = 0
@@ -17,10 +16,7 @@ vim.g.netrw_bufsettings = "nomod nu nobl nowrap ro" -- line numbers in netrw
 -- Tabs and indentation
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.expandtab = true
 vim.opt.smartindent = true
-
-vim.wo.wrap = false
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", function()
