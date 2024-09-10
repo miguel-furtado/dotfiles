11cf136e6eaee65fe5a846312bfc914bf231e2d1
f_miguel <fabiosemedo200@gmail.com>
2024-09-10 03:31:34 +0100

    nvim: use nightfox colorschemes

--- config/nvim/lua/fmiguel/plugins/colors.lua
+++ config/nvim/lua/fmiguel/plugins/colors.lua
@@ -1,1 +1,22 @@
-return { "vimpostor/vim-lumen" }
+function set_colorscheme()
+	if vim.o.background == "dark" then
+		vim.cmd("colorscheme carbonfox")
+	else
+		vim.cmd("colorscheme dayfox")
+	end
+end
+
+return {
+	"vimpostor/vim-lumen",
+	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			vim.api.nvim_create_autocmd("OptionSet", {
+				desc = "Change colorscheme based on system settings",
+				pattern = "background",
+				callback = set_colorscheme,
+			})
+			set_colorscheme()
+		end,
+	},
+}
