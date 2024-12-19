e0a98e24e2cb2e28a03aff7812a692f854f8b0b3
f_miguel <fabiosemedo200@gmail.com>
2024-12-19 23:06:36 +0000

    nvim: configure colorscheme settings

--- config/nvim/lua/fmiguel/plugins/colors.lua
+++ config/nvim/lua/fmiguel/plugins/colors.lua
@@ -1,22 +1,42 @@
-function set_colorscheme()
-	if vim.o.background == "dark" then
-		vim.cmd("colorscheme carbonfox")
-	else
-		vim.cmd("colorscheme dayfox")
-	end
-end
-
 return {
-	"vimpostor/vim-lumen",
+	{
+		"vimpostor/vim-lumen",
+		lazy=false,
+		priority=1000
+	},
 	{
 		"EdenEast/nightfox.nvim",
 		config = function()
+			function set_colorscheme()
+				if vim.o.background == "light" then
+					vim.cmd("colorscheme dayfox")
+				else
+					vim.cmd("colorscheme carbonfox")
+				end
+			end
+
 			vim.api.nvim_create_autocmd("OptionSet", {
 				desc = "Change colorscheme based on system settings",
 				pattern = "background",
 				callback = set_colorscheme,
 			})
+
 			set_colorscheme()
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		enabled = false,
+		config = function()
+			vim.api.nvim_create_autocmd("BufRead", {
+				pattern = {"*.js", "*.ts"},
+				callback = function()
+					require"kanagawa".config.colors.theme.all = {syn = {constant = "none"}}
+				end,
+			})
+
+			vim.cmd("colorscheme kanagawa")
+
 		end,
 	},
 }
