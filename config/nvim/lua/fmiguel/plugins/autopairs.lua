76e7bd571a8db439f0823e103b9b6355cd5c8df9
f_miguel <fabiosemedo200@gmail.com>
2024-12-27 02:01:34 +0000

    nvim: introduce mini
    Replaces autopairs and gitsigns.

--- config/nvim/lua/fmiguel/plugins/autopairs.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"windwp/nvim-autopairs", -- auto-close pairs
-	event = "InsertEnter",
-	opts = {
-		check_ts = true,
-		enable_bracket_in_quote = false,
-	},
-}
---
+++ config/nvim/lua/fmiguel/plugins/mini.lua
@@ -0,0 +1,22 @@
+return {
+	"echasnovski/mini.nvim",
+	version = "0.x.x",
+	lazy = false,
+	keys = { {
+		"<leader>df",
+		function()
+			require("mini.diff").toggle_overlay()
+		end,
+	} },
+	config = function()
+		require("mini.diff").setup({
+			view = {
+				style = "sign",
+				signs = { add = "+", change = "~", delete = "-" },
+			},
+			delay = { text_change = 0 },
+		})
+		require("mini.pairs").setup()
+		vim.cmd("colorscheme minischeme")
+	end,
+}
