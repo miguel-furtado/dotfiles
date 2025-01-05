2c76f81cc53ffcc8a3524ad03fff8d48c259bdf4
f_miguel <fabiosemedo200@gmail.com>
2025-01-05 02:36:33 +0000

    Revert "nvim: introduce mini"
    This reverts commit 76e7bd571a8db439f0823e103b9b6355cd5c8df9.

---
+++ neovim/lua/fmiguel/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+return {
+	"windwp/nvim-autopairs", -- auto-close pairs
+	event = "InsertEnter",
+	opts = {
+		check_ts = true,
+		enable_bracket_in_quote = false,
+	},
+}
---
+++ neovim/lua/fmiguel/plugins/gitsigns.lua
@@ -0,0 +1,13 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	version = "0.x.x",
+	opts = {
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+		},
+	},
+}
