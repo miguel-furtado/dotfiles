a00bb261ce0a3cd325a09d411a8a32ded7d94890
f_miguel <fabiosemedo200@gmail.com>
2025-01-19 19:33:37 +0000

    wezterm: update settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,14 +3,18 @@ local config = {}
 
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 13
-config.initial_rows = 30
-config.initial_cols = 90
+config.initial_rows = 32
+config.initial_cols = 95
 config.harfbuzz_features = { "calt=0" } --disable font ligatures
 config.enable_wayland = false
 config.default_cursor_style = "BlinkingBlock"
 config.cursor_blink_ease_out = "Constant"
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_rate = 600
+
+wezterm.on("gui-startup", function(cmd) -- set startup Window position
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or { position = { x = 40, y = 60 } })
+end)
 
 config.keys = {}
 for i = 1, 8 do
@@ -22,8 +26,8 @@ for i = 1, 8 do
 end
 
 if wezterm.gui.get_appearance() == "Dark" then
-	-- config.color_scheme = "Xterm"
-	config.color_scheme = "Dracula"
+	config.color_scheme = "Xterm"
+--	config.color_scheme = "Dracula"
 else
 	config.color_scheme = "Alabaster"
 end
