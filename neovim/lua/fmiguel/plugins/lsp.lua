48ea947ecd16bd5eb65c155cd557e5ee082e0577
f_miguel <fabiosemedo200@gmail.com>
2025-06-03 21:25:31 +0100

    nvim: exclude declaration in lsp find_references

--- neovim/lua/fmiguel/plugins/lsp.lua
+++ neovim/lua/fmiguel/plugins/lsp.lua
@@ -7,13 +7,16 @@ return {
 			callback = function()
 				vim.keymap.set("n", "gt", vim.lsp.buf.type_definition)
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+				vim.keymap.set("n", "grr", function()
+					vim.lsp.buf.references({ includeDeclaration = false })
+				end)
 			end,
 		})
 
 		local servers =
 			{ "html", "cssls", "ts_ls", "eslint", "jsonls", "dockerls", "pyright", "clangd", "jdtls", "rust_analyzer" }
 		for _, server in ipairs(servers) do
-			require("lspconfig")[server].setup{}
+			require("lspconfig")[server].setup({})
 		end
 	end,
 }
