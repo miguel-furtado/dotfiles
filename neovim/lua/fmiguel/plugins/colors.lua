f7927201b55c0be779bf8ee5405bc6520aac948c
f_miguel <fabiosemedo200@gmail.com>
2025-06-15 23:00:47 +0100

    nvim: run back to nightfox

--- neovim/lua/fmiguel/plugins/colors.lua
+++ neovim/lua/fmiguel/plugins/colors.lua
@@ -4,4 +4,26 @@ return {
 		lazy = false,
 		priority = 1000,
 	},
+	{
+		"EdenEast/nightfox.nvim",
+		version = "^3",
+		config = function()
+			function set_colorscheme()
+				if vim.o.background == "light" then
+					vim.cmd("colorscheme dawnfox")
+				else
+					vim.cmd("colorscheme carbonfox")
+				end
+			end
+
+			set_colorscheme()
+			vim.api.nvim_create_autocmd("OptionSet", {
+				desc = "Update colorscheme on background change",
+				pattern = "background",
+				callback = function()
+					set_colorscheme()
+				end,
+			})
+		end
+	}
 }
